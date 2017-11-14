2e55ebb7d66c54c22465712423ae151ee3d7e99b
Jesus Valadez <kzkz98@outlook.com>
2017-11-14 00:22:35 -0600

    Clases basicas de personajes creadas (Base, Torre, Enemigo)

--- src/Gui/GameFrame.java
+++ src/Gui/GameFrame.java
@@ -6,8 +6,14 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class GameFrame implements Initializable {
+
+    public GameFrame () {
+
+    }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+
 
     }
 }
